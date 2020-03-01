352ce5c6716824cddcd010234b5b2ca5690bd2f2
kondorskiy <kondorskiy@gmail.com>
2020-03-01 22:13:13 +0300

    New subroutines to plot cylinder of 2D polar plots of some function "func(phi, a)" with a scan parameter, "a", varying along the cylinder axis.

--- adk_plotlib.cpp
+++ adk_plotlib.cpp
@@ -63,6 +63,55 @@ void print2Ddata(
 
 
 /*--------------------------------------------------------------------
+  Print tabulated 3D polar surface for function "func(phi, a)"
+  with a scan parameter, a, varying along the cylinder axis.
+  ARGUMENT phi = 0..360. SHOULD BE IN DEGREES! a - is the parameter.
+--------------------------------------------------------------------*/
+void print2DdataScanXYZ(
+  double (*func)(double, double), // Function to plot.
+  const int &x_num,             // Number of argument points.
+  const double &a_min,          // Minimal value of the parameter.
+  const double &a_max,          // Maximal value of the parameter.
+  const int &a_num,             // Number of points for the parameter.
+  const std::string &name,      // Data identification name.
+  double &r_min,                // Result minimal value of function.
+  double &r_max)                // Result maximal value of function.
+{
+  std::string file_name = name + ".dat";
+  std::ofstream fout(file_name.c_str(), std::ios::out);
+
+  double d2r = M_PI/180.0;
+
+  for(int iz = 0; iz < a_num; ++iz) {     // Parameter.
+    double z = a_min + iz*(a_max - a_min)/(a_num - 1);
+
+    for(int ia = 0; ia < x_num; ++ia) {    // Azimuth.
+      double phi = ia*360.0/(x_num - 1);
+
+      double r = (*func)(phi, z);
+
+      double x = r*cos(phi*d2r);
+      if( (ia == 0) && (iz == 0) ) {
+        r_min = x;
+        r_max = x;
+      } else {
+        if(x < r_min) r_min = x;
+        if(x > r_max) r_max = x;
+      }
+
+      double y = r*sin(phi*d2r);
+      if(y < r_min) r_min = y;
+      if(y > r_max) r_max = y;
+
+      fout << x << " " << y << " " << z << "\n";
+    }                                       // Azimuth.
+    fout << "\n";
+  }                                       // Parameter.
+  fout.close();
+}
+
+
+/*--------------------------------------------------------------------
   Print tabulated 3D function "func" to the file.
 --------------------------------------------------------------------*/
 void print3Ddata(
@@ -263,6 +312,125 @@ void GNUplot2Dpolar(
   fout << "set style line 1 lt 1 lc 1 lw 4\n";
   fout << "plot \"" << short_name
     << ".dat\" u 1:2 w l ls 1\n";
+  fout.close();
+
+  std::string command = "gnuplot " + file_name;
+  system(command.c_str());
+  command = "rm " + file_name;
+  system(command.c_str());
+}
+
+
+/*--------------------------------------------------------------------
+  Run GNUplot script to plot 3D cylinder of 2D polar plots of some
+  function "func(phi, a)" with a scan parameter, a, varying along
+  the cylinder axis.
+--------------------------------------------------------------------*/
+void GNUplot2DpolarScan(
+  const std::string &short_name,  // Data file name without extension.
+  const double &r_min,            // Result minimal value of function.
+  const double &r_max,            // Result maximal value of function.
+  const double &a_min,            // Minimal value of the parameter.
+  const double &a_max,            // Maximal value of the parameter.
+  const std::string &title)       // Title of the figure.
+{
+  std::string file_name = "plot.plt";
+  std::ofstream fout(file_name.c_str(), std::ios::out);
+
+  fout << "set terminal pngcairo size " 
+    << X_SIZE << "," << X_SIZE << " font \",24\"\n";
+
+  fout << "set title \"" << title << "\"\n";
+  fout << "set angles degrees\n";
+  fout << "set style line 1 lc rgb \'#606060\' lt 1 lw 1\n";
+  fout << "set pm3d depthorder hidden3d 1\n";
+  fout << "set hidden3d\n";
+  fout << "set style fill transparent solid 0.75\n";
+  // fout << "set palette rgb 9,9,3\n";
+  fout << "set palette rgbformulae 22,13,-31\n";
+  fout << "unset colorbox\n";
+  fout << "unset key\n";
+  // fout << "set border 4095\n";
+  fout << "unset border\n";
+  fout << "unset tics\n";
+  fout << "set ticslevel 0\n";
+  fout << "set isosamples 50,100\n";
+
+  fout << "set style line 2 lc rgb \'#404040\' lt 1 lw 3\n";
+  fout << "set style arrow 1 nohead ls 2\n";
+
+  double scl = 1.3;
+  fout << "set xrange [" << scl*r_min << ":" << scl*r_max << "]\n";
+  fout << "set yrange [" << scl*r_min << ":" << scl*r_max << "]\n";
+  fout << "set zrange [" << a_min - 0.2*(a_max + a_min) << ":" 
+    << a_max + 0.2*(a_max + a_min) << "]\n";
+
+  // fout << "set xyplane at " << 0.5*(a_max + a_min) << "\n";
+
+  fout << "set_label(x, text, z, s) = sprintf";
+  fout << "(\"set label \'%s\' at (" << 1.2*r_max << "*cos(%f)), ";
+  fout << "(" << 1.2*r_max 
+    << "*sin(%f)), %f center %s\", text, x, x, z, s)\n";
+
+  fout << "set style line 3 lc rgb \'#660000\' lt 1 lw 8\n";
+  fout << "set style arrow 2 nohead ls 3\n";
+  fout << "set arrow from 0, 0, " << a_min - 0.4*(a_max + a_min);
+  fout << " to 0, 0, " << a_max << " as 2\n";
+  fout << "set arrow from 0, 0, " << a_max << " to 0, 0, ";
+  fout << a_max + 0.4*(a_max + a_min) << " as 2 front\n";
+
+  fout << "set_line(x, z, s) = sprintf";
+  fout << "(\"set arrow from (" << -1.1*r_max << "*cos(%f)), ";
+  fout << "(" << -1.1*r_max << "*sin(%f)), %f to (" << 1.1*r_max 
+    << "*cos(%f)), " << "(" << 1.1*r_max;
+  fout << "*sin(%f)), %f as 1 %s\", x, x, z, x, x, z, s)\n";
+  double z = a_min;
+  fout << "eval set_line(0, " << z << ", \"\")\n";
+  fout << "eval set_line(45, " << z << ", \"\")\n";
+  fout << "eval set_line(90, " << z << ", \"\")\n";
+  fout << "eval set_line(135, " << z << ", \"\")\n";
+  fout << "eval set_label(0, \"0\", " << z << ", \"\")\n";
+  fout << "eval set_label(45, \"45\", " << z << ", \"\")\n";
+  fout << "eval set_label(90, \"90\", " << z << ", \"\")\n";
+  fout << "eval set_label(135, \"135\", " << z << ", \"\")\n";
+  fout << "eval set_label(180, \"180\", " << z << ", \"\")\n";
+  fout << "eval set_label(225, \"225\", " << z << ", \"\")\n";
+  fout << "eval set_label(270, \"270\", " << z << ", \"\")\n";
+  fout << "eval set_label(315, \"315\", " << z << ", \"\")\n";
+  z = a_max;
+  fout << "eval set_line(0, " << z << ", \"front\")\n";
+  fout << "eval set_line(45, " << z << ", \"front\")\n";
+  fout << "eval set_line(90, " << z << ", \"front\")\n";
+  fout << "eval set_line(135, " << z << ", \"front\")\n";
+  fout << "eval set_label(0, \"0\", " << z << ", \"front\")\n";
+  fout << "eval set_label(45, \"45\", " << z << ", \"front\")\n";
+  fout << "eval set_label(90, \"90\", " << z << ", \"front\")\n";
+  fout << "eval set_label(135, \"135\", " << z << ", \"front\")\n";
+  fout << "eval set_label(180, \"180\", " << z << ", \"front\")\n";
+  fout << "eval set_label(225, \"225\", " << z << ", \"front\")\n";
+  fout << "eval set_label(270, \"270\", " << z << ", \"front\")\n";
+  fout << "eval set_label(315, \"315\", " << z << ", \"front\")\n";
+
+  /* fout << "set output \"" << short_name << "-rp0.png\"\n";
+  fout << "set view 60,0,1.5,1\n";
+  fout << "splot \"" << short_name << ".dat\" w pm3d\n"; */
+
+  fout << "set output \"" << short_name << "-rp1.png\"\n";
+  fout << "set view 60,72,1.5,1\n";
+  fout << "splot \"" << short_name << ".dat\" w pm3d\n";
+
+  fout << "set output \"" << short_name << "-rp2.png\"\n";
+  fout << "set view 60,144,1.5,1\n";
+  fout << "splot \"" << short_name << ".dat\" w pm3d\n";
+
+  fout << "set output \"" << short_name << "-rp3.png\"\n";
+  fout << "set view 60,216,1.5,1\n";
+  fout << "splot \"" << short_name << ".dat\" w pm3d\n";
+
+  fout << "set output \"" << short_name << "-rp4.png\"\n";
+  fout << "set view 60,288,1.5,1\n";
+  fout << "splot \"" << short_name << ".dat\" w pm3d\n";
+
   fout.close();
 
   std::string command = "gnuplot " + file_name;
@@ -537,10 +705,10 @@ void plot2Dsimple(
 --------------------------------------------------------------------*/
 void plot2Dpolar(
   double (*func)(double),       // Function to plot.
-  const int &x_num,              // Number of argument points.
+  const int &x_num,             // Number of argument points.
   const std::string &name,      // Data identification name.
   const std::string &title,     // Title of the figure.
-  const bool &is_eps)            // Flag to plot additional EPS figure.
+  const bool &is_eps)           // Flag to plot additional EPS figure.
 {
   double f_min, f_max;
 
@@ -551,6 +719,32 @@ void plot2Dpolar(
   if(is_eps)
     GNUplot2Dpolar(name, f_max, title, true);
   GNUplot2Dpolar(name, f_max, title, false);
+}
+
+
+/*--------------------------------------------------------------------
+  Plot cylinder of 2D polar plots of some function "func(phi, a)"
+  with a scan parameter, a, varying along the cylinder axis.
+  ARGUMENT phi SHOULD BE IN DEGREES! a - is the parameter.
+--------------------------------------------------------------------*/
+void plot2DpolarScan(
+  double (*func)(double, double), // Function to plot.
+  const int &x_num,             // Number of argument points.
+  const double &a_min,          // Minimal value of the parameter.
+  const double &a_max,          // Maximal value of the parameter.
+  const int &a_num,             // Number of points for the parameter.
+  const std::string &name,      // Data identification name.
+  const std::string &title)     // Title of the figure.
+{
+  double r_min, r_max;
+
+  // Print the data to plot.
+  print2DdataScanXYZ(func, x_num,
+    a_min, a_max, a_num, name, r_min, r_max);
+
+  // Plot using GNUplot script.
+  GNUplot2DpolarScan(name, r_min, r_max,
+    a_min, a_max, title);
 }
 
 
@@ -676,6 +870,16 @@ double my_func2(double x)
 
 
 /*--------------------------------------------------------------------
+  Test 2D function with parameter.
+--------------------------------------------------------------------*/
+double my_func2scan(double x, double a)
+{
+  double d2r = M_PI/180.0;
+  return pow(cos(x*d2r + a), 2);
+}
+
+
+/*--------------------------------------------------------------------
   Test 3D function.
 --------------------------------------------------------------------*/
 double my_func3(double x, double y)
@@ -699,6 +903,12 @@ int main(int argc, char **argv)
      ARGUMENT SHOULD BE IN DEGREES!   */
   plot2Dpolar(my_func2, 180, "Test2Dpolar",
    "Test for 2D polar plot", true);
+
+  /* Plot cylinder of 2D polar plots of some function "func(phi, a)"
+     with a scan parameter, a, varying along the cylinder axis.
+     ARGUMENT phi SHOULD BE IN DEGREES! a - is the parameter. */
+  plot2DpolarScan(my_func2scan, 90, 0.0, 1.57, 16, "Test2DpolarScan",
+    "Test for cylinder of 2D polar plots with a scan parameter");
 
   // Plot 3D colormap for some function "func".
   plot3Dcolormap(my_func3, 0.0, 360.0, 180, 0.0, 180.0, 90,
---
+++ run.sh
@@ -0,0 +1,3 @@
+g++ -o adk_plotlib.exe adk_plotlib.cpp;
+./adk_plotlib.exe;
+rm adk_plotlib.exe;
